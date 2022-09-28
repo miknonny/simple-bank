3fb1187b27dca713fd1473534531f746fa27fd8e
Mbah Chukwunomso <miknonny@gmail.com>
2022-09-28 14:04:09 +0100

    docker file

--- api/account.go
+++ api/account.go
@@ -68,7 +68,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	if account.Owner != authPayload.Username {
-		err := errors.New("account doesn't belong to the authenticated user")
+		err := errors.New("account doesn't belong to the authenticated/loggedin user")
 		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
 		return
 	}
